11a74f75f3802f6bfcb0d233171f5b77ac9a452a
Frank Thomsen <frtho@sdfi.dk>
2023-11-21 10:09:40 +0100

    make theme toggle remember choice

--- assets/designsystem.js
+++ assets/designsystem.js
@@ -39,6 +39,7 @@ customElements.define("code-view", CodeView);
 
 // src/js/themeToggle.js
 var ThemeToggle = class extends HTMLElement {
+  localstorageKey = "SDFI-theme-choice";
   buttonDark;
   buttonLight;
   style = `
@@ -51,11 +52,21 @@ var ThemeToggle = class extends HTMLElement {
     button[data-toggle="light"] {
       background-color: var(--c1);
     }
+    button[data-toggle="light"]:hover,
+    button[data-toggle="light"]:active {
+      background-color: var(--c2);
+      border-color: var(--c2);
+    }
     button[data-toggle="light"] > svg {
       --ds-icon-color: var(--c10);
     }
     button[data-toggle="dark"] {
       background-color: var(--c6);
+    }
+    button[data-toggle="dark"]:hover,
+    button[data-toggle="dark"]:active {
+      background-color: var(--c5);
+      border-color: var(--c5);
     }
     button[data-toggle="dark"] > svg {
       --ds-icon-color: var(--c1);
@@ -94,11 +105,22 @@ var ThemeToggle = class extends HTMLElement {
   }
   goDarkHandler(event) {
     document.documentElement.dataset.theme = "dark";
+    localStorage.setItem(this.localstorageKey, "dark");
   }
   goLightHandler(event) {
     document.documentElement.dataset.theme = "light";
+    localStorage.setItem(this.localstorageKey, "light");
+  }
+  checkPreference() {
+    const preference = localStorage.getItem(this.localstorageKey);
+    if (preference === "dark") {
+      document.documentElement.dataset.theme = "dark";
+    } else if (preference === "light") {
+      document.documentElement.dataset.theme = "light";
+    }
   }
   connectedCallback() {
+    this.checkPreference();
     this.render();
     this.buttonDark = this.querySelector('button[data-toggle="dark"]');
     this.buttonLight = this.querySelector('button[data-toggle="light"]');
--- src/js/themeToggle.js
+++ src/js/themeToggle.js
@@ -1,5 +1,6 @@
 export class ThemeToggle extends HTMLElement {
 
+  localstorageKey = 'SDFI-theme-choice'
   buttonDark
   buttonLight
   style = `
@@ -12,11 +13,21 @@ export class ThemeToggle extends HTMLElement {
     button[data-toggle="light"] {
       background-color: var(--c1);
     }
+    button[data-toggle="light"]:hover,
+    button[data-toggle="light"]:active {
+      background-color: var(--c2);
+      border-color: var(--c2);
+    }
     button[data-toggle="light"] > svg {
       --ds-icon-color: var(--c10);
     }
     button[data-toggle="dark"] {
       background-color: var(--c6);
+    }
+    button[data-toggle="dark"]:hover,
+    button[data-toggle="dark"]:active {
+      background-color: var(--c5);
+      border-color: var(--c5);
     }
     button[data-toggle="dark"] > svg {
       --ds-icon-color: var(--c1);
@@ -59,13 +70,25 @@ export class ThemeToggle extends HTMLElement {
 
   goDarkHandler(event) {
     document.documentElement.dataset.theme = 'dark'
+    localStorage.setItem(this.localstorageKey, 'dark')
   }
 
   goLightHandler(event) {
     document.documentElement.dataset.theme = 'light'
+    localStorage.setItem(this.localstorageKey, 'light')
+  }
+
+  checkPreference() {
+    const preference = localStorage.getItem(this.localstorageKey)
+    if (preference === 'dark') {
+      document.documentElement.dataset.theme = 'dark'
+    } else if (preference === 'light') {
+      document.documentElement.dataset.theme = 'light'
+    }
   }
 
   connectedCallback() {
+    this.checkPreference()
     this.render()
     this.buttonDark = this.querySelector('button[data-toggle="dark"]')
     this.buttonLight = this.querySelector('button[data-toggle="light"]')
