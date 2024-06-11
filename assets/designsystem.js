cc4a8256245448dd20a7973fc02de84fdf6b72a7
Frank R Thomsen <frtho@sdfi.dk>
2024-06-11 15:53:58 +0200

    improved styles for logos

--- assets/designsystem.js
+++ assets/designsystem.js
@@ -556,54 +556,105 @@ var DSLogoTitle = class extends HTMLElement {
     this.shadow.innerHTML = `
       <style>
         :host {
-          width: 100%;
-          height: 4rem;
+          display: inline-block;
+        }
+        :host(.black) ds-logo {
+          --ds-logo-color: var(--hvid);
+          --ds-logo-outline-color: var(--sort);
+          --ds-logo-background-color: var(--sort);
+        }
+        :host(.white) ds-logo {
+          --ds-logo-color: var(--sort);
+          --ds-logo-outline-color: var(--hvid);
+          --ds-logo-background-color: var(--hvid);
+        }
+        :host(.transparent) ds-logo {
+          --ds-logo-color: var(--ds-logo-theme-color);
+          --ds-logo-outline-color: var(--ds-logo-theme-color);
+          --ds-logo-background-color: transparent;
+        }  
+        .logo-title-wrapper {
+          height: 100%;
+          min-height: 4rem;
           display: flex;
-          position: relative;
-          
           flex-flow: row nowrap;
-          container-type: size;
+          justify-content: flex-start;
+          align-items: flex-start;
+          gap: var(--space-sm);
         }
         ds-logo {
-          height: 100%;
-          width: auto;
-          margin: 0 0.75rem 0 -0.75rem;
-          flex: 0 0 auto;
+          height: 4rem;
+          width: 4rem;
+          margin-left: -0.8rem;
         }
-        div {
-          flex: 0 1 100%;
-          padding-top: 1.33rem;
+        p {
+          margin: 0.75rem 0 0;
+          padding: 0;
         }
-        strong,
-        span {
-          display: block;
+        .single-line {
+          margin-top: 1.33rem;
+          font-weight: 370;
+          font-size: 1.125rem;
+          line-height: 1.33;
         }
         strong {
-          font-weight: 600;
-          font-size: 1rem;
+          font-size: 1.188rem;
+          font-weight: 500;
+          line-height: 1.10;
+          letter-spacing: 0.01rem;
         }
-        span {
-          font-size: .9rem;
+        small {
+          display: inline-block;
+          font-size: 0.875rem;
+          font-weight: 390;
+          line-height: 1.21;
+          padding-bottom: var(--space-xs);
         }
 
-        @container (max-height: 3.9rem) {
-          :host {
-            margin-left: -0.5rem;
-          }
-          ds-logo {
-            margin: 0 0.6rem 0 -0.6rem;
-          }
-          div {
-            padding-top: 0.4rem;
-          }
+        :host(.small) .logo-title-wrapper {
+          min-height: 2rem;
+          gap: var(--space-xs);
+        }
+        :host(.small) ds-logo {
+          height: 1.5rem;
+          width: 1.5rem;
+          margin-left: -0.33rem;
+        }
+        :host(.small) p {
+          margin-top: var(--space-xs);
+        }
+        :host(.small) .single-line {
+          margin-top: var(--space-xs);
+        }
+        :host(.small) .single-line,
+        :host(.small) strong {
+          font-size: 0.875rem;
+          font-weight: 600;
+          line-height: 1.14;
+          letter-spacing: 0.02rem;
+        }
+        :host(.small) small {
+          font-size: 0.875rem;
+          font-weight: 390;
+          line-height: 1.21;
         }
       </style>
-      <ds-logo></ds-logo>
-      <div>
-        <strong>${this.title ? this.title : "SDFI"}</strong>
-        <span>${this.getAttribute("byline") ? this.getAttribute("byline") : "Styrelsen for Dataforsyning og Infrastruktur"}</span>
+      <div class="logo-title-wrapper">
+        <ds-logo></ds-logo>
+        ${this.formatLogoText(this.title, this.getAttribute("byline"))}
       </div>
     `;
+  }
+  formatLogoText(title, byline) {
+    let txt = "";
+    if (title && byline) {
+      txt = `<p><strong>${title}</strong><br><small>${byline}</small></p>`;
+    } else if (title && !byline) {
+      txt = `<p class="single-line">${title}</p>`;
+    } else {
+      txt = `<p class="single-line">Styrelsen for Dataforsyning og Infrastruktur</p>`;
+    }
+    return txt;
   }
 };
 
