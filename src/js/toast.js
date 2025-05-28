c1f32ab2a4e4318f6221a5249df76022910d0a2a
Johan Lund Neergaard <johanlundn@gmail.com>
2025-05-28 10:55:18 +0200

    add a close button to the toast

--- src/js/toast.js
+++ src/js/toast.js
@@ -1,8 +1,19 @@
 export function showToast({message, duration = 5000}) {
   const newToast = document.createElement('div')
+  const paragraph = document.createElement('p')
+  const closeButton = document.createElement('button')
+  const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+  const use = document.createElementNS('http://www.w3.org/2000/svg', 'use')
   newToast.className = 'ds-toast-item'
-  newToast.innerText = message
   newToast.dataset.theme = 'dark'
+  paragraph.innerText = message
+  closeButton.className = 'quiet'
+  closeButton.title = 'Luk'
+  use.setAttribute('href', '../assets/icons.svg#close')
+  svg.appendChild(use)
+  closeButton.appendChild(svg)
+  newToast.appendChild(paragraph)
+  newToast.appendChild(closeButton)
 
   const targetElement = document.querySelector('.ds-toast-container')
   if (!targetElement) {
@@ -13,6 +24,8 @@ export function showToast({message, duration = 5000}) {
   } else {
     targetElement.append(newToast)
   }
+
+  closeButton.addEventListener('click', () => newToast.remove())
   
   setTimeout(function() {
     newToast.remove()
